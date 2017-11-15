895fa16152149bb47b0fb7b4724a3d8cf1422ded
shailen28 <shailenster@gmail.com>
2017-11-15 15:49:27 -0600

    app complete

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var app = require('express')();          // initializes app 
 var http = require('http').Server(app);  // app is function handler for HTTP server
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){		 // '/' is route handler
 	res.sendFile(__dirname + '/index.html');  // send my index.html file as a template when get request is made to homepage"
 });
 
+io.on('connection', function(socket){
+	console.log('a user has connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	}) 
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	}); 
+});   
+
 http.listen(8080, function(){
 	console.log('listening on *:8080');  // http server listens on port 3000 and terminal outputs
 });
-
 
